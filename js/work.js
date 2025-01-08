addd9be9a55b14938047a3700c78f4e64243617e
qwerty <145857108+rubakhaanastasia@users.noreply.github.com>
2025-01-09 00:43:52 +0100

    Add files via upload

--- js/work.js
+++ js/work.js
@@ -15,7 +15,6 @@ for (let key of userKeys) {
   console.log(key);
 }
 
-
 //2.
 function countProps(obj) {
   const keys = Object.keys(obj);
@@ -29,9 +28,8 @@ const member = {
 
 console.log(countProps(user));
 
-
 //3.
-const employees = [
+const emplosees = [
   {
     lilit: 10,
     salary: 17000,
@@ -57,48 +55,103 @@ const employees = [
     salary: 22000,
   },
 ];
-function findBestEmployee(employees) {
-  let totalSalary = 0;
-  for (const employee of Object.key(employees)) {
-    totalSalary += employee.salary;
-  }
-  return totalSalary;
-}
-console.log(findBestEmployee(employees));
-
+// function findBestEmployee(employees) {
+//   let totalSalary = 0;
+//   for (const employee of Object.key(employees)) {
+//     totalSalary += employee.salary;
+//   }
+//   return totalSalary;
+// }
+// console.log(findBestEmployee(emplosees));
 
 //4.
-function countTotalSalary(employees) {
+const emploees = [
+  {
+    leo: 15000,
+  },
+  {
+    emma: 12000,
+  },
+  {
+    anastasia: 14500,
+  },
+  {
+    meddy: 10000,
+  },
+  {
+    lily: 22000,
+  },
+];
+function countTotalSalary(emploees) {
   let total = 0;
-  for (const employee of Object.values(employees)) {
-    total += salaries[employee];
+  for (let employee of emploees) {
+    total += employee.salary;
   }
   return total;
 }
-const employeeSalaries = {
-    andrii: 15100,
-    serhii: 14300,
-    anastasia: 14500,
-    tetiana: 8700
-};
-console.log(countTotalSalary(employees));
+console.log(countTotalSalary(emploees));
 
+//5.
+function getAllPropValues(arr, prop) {
+  const values = [];
+  for (let employee of arr) {
+    if (Object.keys(employee).includes(prop)) {
+      values.push(employee[prop]);
+    }
+  }
+  return values;
+}
+
+const employees = [
+  {
+    man: "worked",
+  },
+  {
+    man1: "notworked",
+  },
+  {
+    man2: "worked",
+  },
+  {
+    man3: "notworked",
+  },
+  {
+    man4: "worked",
+  },
+];
+
+console.log(getAllPropValues(employees, "man"));
+console.log(getAllPropValues(employees, "man1"));
+console.log(getAllPropValues(employees, "man2"));
 
 //6.
 const basket = [
-  { name: "kiwi", for1: 5 },
-  { name: "apple", for1: 25 },
-  { name: "cherry", for1: 100 },
-  { name: "cherrry", for1: 150 },
+  {
+    sport: "volleyball",
+    price: 100,
+    amount: 3,
+  },
+  {
+    sport: "volleyball",
+    price: 60,
+    amount: 4,
+  },
+  {
+    sport: "football",
+    price: 140,
+    amount: 5,
+  },
 ];
-function calculateTotalPrice(allProducts, productName, weight) {
-  let price = 0;
-  for (const product of allProducts) {
-    if (product.name === productName) {
-      price = product.for1 * weight;
+function calculateTotalPrice(productsArray, searchName) {
+  let totalCost = 0;
+  for (let product of productsArray) {
+    if (product.sport === searchName) {
+      totalCost += product.price * product.amount;
     }
   }
-  return price;
+  return totalCost;
 }
-console.log(calculateTotalPrice(basket, "kiwi", 7));
-console.log(calculateTotalPrice(basket, "cherry", 8)); 
+console.log(calculateTotalPrice(productsArray, "volleyball", 2));
+console.log(calculateTotalPrice(searchName, "football", 4));
+// Напиши функцію calculateTotalPrice(allProdcuts, productName), яка отримує масив об'єктів та ім'я продукту (значення властивості name). Повертає загальну вартість продукту (ціна * кількість).
+// Викличи функції для перевірки працездатності твоєї реалізації.
